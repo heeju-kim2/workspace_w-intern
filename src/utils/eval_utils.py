143c859e7102cd84f9e91762fec6aca0e699205c
root <root@nlp-part-dylee-0.xla-test.personal-ws-intern-daeyeop-lee.svc.cluster.local>
2024-07-17 10:44:40 +0000

    rouge score calculation modified & working well

--- src/utils/eval_utils.py
+++ src/utils/eval_utils.py
@@ -2,14 +2,28 @@ import copy
 import torch
 import evaluate
 from tqdm import tqdm
+import numpy as np
 from transformers.data import DataCollatorForSeq2Seq
+from utils.config_utils import get_dataloader_kwargs
 from dataset_srcs.samsum_dataset import get_preprocessed_samsum_for_rouge
 
 
 def rouge_for_samsum(train_config, model, tokenizer, accelerator, wandb_run):
     rouge_dataset, summaries = get_preprocessed_samsum_for_rouge(dataset_config=None, tokenizer=tokenizer)
+    lengths = [len(d['input_ids']) for d in rouge_dataset]
+    ids = np.argsort(lengths, kind='mergesort')
+
     metric = evaluate.load('rouge')
-    rouge_loader = torch.utils.data.DataLoader(rouge_dataset, pin_memory=True, batch_size=train_config.eval_batch_size, collate_fn=DataCollatorForSeq2Seq(tokenizer=tokenizer))
+
+    rouge_dl_kwargs = get_dataloader_kwargs(train_config, rouge_dataset, tokenizer, "val")
+    rouge_loader = torch.utils.data.DataLoader(
+        rouge_dataset,
+        num_workers=train_config.num_workers_dataloader,
+        pin_memory=True,
+        **rouge_dl_kwargs,
+    )
+
+    # rouge_loader = torch.utils.data.DataLoader(rouge_dataset, pin_memory=True, batch_size=train_config.eval_batch_size, collate_fn=DataCollatorForSeq2Seq(tokenizer=tokenizer), shuffle=False)
 
     idx = 0
     for step, batch in enumerate(tqdm(rouge_loader, desc="calculating Rouge")):
@@ -28,7 +42,7 @@ def rouge_for_samsum(train_config, model, tokenizer, accelerator, wandb_run):
         references = []
         for i, p in zip(input_prompts, output_prompts):
             predictions.append(p[len(i):])
-            references.append(summaries[idx])
+            references.append(summaries[ids[idx]])
             idx += 1
 
         metric.add_batch(
